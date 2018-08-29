aa7849c2bbf523ea1034e739c0e593b7e19cd080
Burak Gümüş <burakgumusfb@gmail.com>
2018-08-29 09:17:36 +0300

    f

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var constants = require('./public/js/constants');
 var Drone = require('./public/js/drone');
 var fetch = require('node-fetch');
 var droneobject = require('./public/js/data');
+const hexRgb = require('hex-rgb');
 
 let imageData = [
   [0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF, 0xFF0000FF],
@@ -22,46 +23,22 @@ app.get('/', function (req, res) {
     drone.createDrone();
 
   }
-  function HEXToVBColor(rrggbb) {
-    var bbggrr = rrggbb.substr(4, 2) + rrggbb.substr(2, 2) + rrggbb.substr(0, 2);
-    return parseInt(bbggrr, 16);
-  }
 
-  function VBColorToHEX(i) {
-    var hex = (i & 0xFF).toString(16) +
-      ((i >> 8) & 0xFF).toString(16) +
-      ((i >> 16) & 0xFF).toString(16) +
-      ((i >> 24) & 0xFF).toString(16);
 
-    hex += '000000'; // pad result
-    hex = hex.substring(0, 6);
-    return "#" + hex;
-  }
-
-  setInterval(function () {
+  let x = setInterval(function () {
     if (dronecount == Object.keys(droneobject).length) {
 
       Object.keys(droneobject).forEach(function (key) {
+
         let val = droneobject[key];
         var d = val.split(';');
-        console.log(d[0] + " ıncı");
-        let image = new Jimp(15, 15, function (err, image) {
 
+        var l = hexRgb(d[0]);
+        console.log(l["red"]);
+        let image = new Jimp(1000, 1000, function (err, image) {
 
-          var l =  Jimp.rgbaToInt(255, 255, 255, 255);
-          var c = VBColorToHEX(0xFF0000FF)
-          console.log(l);
-          image.setPixelColor(0xFF00FF, 0, 0);
-          image.setPixelColor(0xFF00FF, 1, 0);
-          image.setPixelColor(0xFF00FF, 2, 0);
-          image.setPixelColor(0xFF00FF, 3, 0);
-          image.setPixelColor(0xFF00FF, 4, 0);
-          image.setPixelColor(0xFF00FF, 5, 0);
-          image.setPixelColor(0xFF00FF, 6, 0);
-          image.setPixelColor(0xFF00FF, 7, 0);
-          image.setPixelColor(0xFF00FF, 8, 0);
-          image.setPixelColor(0xFF00FF, 9, 0);
-          image.setPixelColor(0xFF00FF, 10, 0);
+          var xx = Jimp.rgbaToInt(l["red"], l["blue"], l["green"], 255);
+          image.setPixelColor(xx, 0, 0);
 
           image.write('dronemap.png', (err) => {
             if (err) throw err;
@@ -70,7 +47,8 @@ app.get('/', function (req, res) {
 
       });
     }
-  }, 1000)
+    clearInterval(x);
+  }, 500)
 
 
   res.sendFile(path.join(__dirname + '/index.html'));
